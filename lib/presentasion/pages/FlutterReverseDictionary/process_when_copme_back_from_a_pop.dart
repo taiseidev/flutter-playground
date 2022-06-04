36856d56073d19ff60a60fdaed585ad665d784e2
onishi taisei <taisei.onishi@axia-works.com>
2022-06-05 02:08:39 +0900

    learn structure process when come back from a pop

--- lib/presentasion/pages/FlutterReverseDictionary/process_when_copme_back_from_a_pop.dart
+++ lib/presentasion/pages/FlutterReverseDictionary/process_when_copme_back_from_a_pop.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_playground/presentasion/pages/FlutterReverseDictionary/want_to_do_hot_reload_in_dialog.dart';
 
 class ProcessWhenComeBackFromAPop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async => await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => WantToDoHotReloadInDialog(),
+                ),
+              ).then((value) {
+                print(value);
+              }),
+              child: Text('await pop'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
