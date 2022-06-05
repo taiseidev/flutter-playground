2e8d00e24afa0395574ce093d9457148c854265d
onishi taisei <taisei.onishi@axia-works.com>
2022-06-05 11:32:37 +0900

    create string to enum file

--- lib/presentasion/pages/FlutterReverseDictionary/flutter_reverse_dictionary.dart
+++ lib/presentasion/pages/FlutterReverseDictionary/flutter_reverse_dictionary.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/presentasion/pages/FlutterReverseDictionary/process_when_copme_back_from_a_pop.dart';
+import 'package:flutter_playground/presentasion/pages/FlutterReverseDictionary/strint_to_enum.dart';
 import 'package:flutter_playground/presentasion/pages/FlutterReverseDictionary/want_to_do_hot_reload_in_dialog.dart';
 
-class FlutterReverseDictionary extends StatelessWidget {
+class FlutterReverseDictionary extends StatefulWidget {
+  @override
+  State<FlutterReverseDictionary> createState() =>
+      _FlutterReverseDictionaryState();
+}
+
+class _FlutterReverseDictionaryState extends State<FlutterReverseDictionary> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +43,15 @@ class FlutterReverseDictionary extends StatelessWidget {
                 ),
               ),
               child: const Text('process when come back from a pop'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => StrintToEnum(),
+                ),
+              ),
+              child: const Text('Strint to enum'),
             ),
           ],
         ),
---
+++ lib/presentasion/pages/FlutterReverseDictionary/strint_to_enum.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_playground/presentasion/pages/FlutterReverseDictionary/want_to_do_hot_reload_in_dialog.dart';
+
+class StrintToEnum extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
