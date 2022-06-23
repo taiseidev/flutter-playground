73c3156977ca157e995ec18e4911ed80979d884b
onishi taisei <taisei.developer@gmail.com>
2022-06-23 23:13:36 +0900

    key学習用に二つのContainerと状態変更用のFloatingActionButtonを設置

--- lib/presentasion/pages/playground/learning_key.dart
+++ lib/presentasion/pages/playground/learning_key.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class LearningKey extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.red,
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
+    );
   }
 }
