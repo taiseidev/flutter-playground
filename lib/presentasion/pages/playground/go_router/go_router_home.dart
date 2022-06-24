ba2e936efae7545ffd695b2bb5df82df297bf039
onishi taisei <taisei.developer@gmail.com>
2022-06-24 23:02:28 +0900

    1画面に画面遷移できるように実装。

--- lib/presentasion/pages/playground/go_router/go_router_home.dart
+++ lib/presentasion/pages/playground/go_router/go_router_home.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class GoRouterHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            // context.go('/1')でも挙動は同じ
+            onPressed: () => GoRouter.of(context).go('/1'),
+            child: const Text('1画面に遷移')),
+      ),
+    );
   }
 }
