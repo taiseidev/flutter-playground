c88c970035d64ad9c76dae141bfa5f2b937c5734
onishi taisei <taisei.developer@gmail.com>
2022-06-24 22:59:05 +0900

    GoRouterの定義してアプリ起動時にホーム画面に遷移できるように実装

--- lib/presentasion/pages/playground/go_router/go_router_sample.dart
+++ lib/presentasion/pages/playground/go_router/go_router_sample.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_playground/presentasion/pages/playground/go_router/go_router1.dart';
+import 'package:flutter_playground/presentasion/pages/playground/go_router/go_router_home.dart';
+import 'package:go_router/go_router.dart';
+
+void main() {
+  runApp(GoRouterSample());
+}
 
 class GoRouterSample extends StatelessWidget {
+  GoRouterSample({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+  Widget build(BuildContext context) => MaterialApp.router(
+        routeInformationProvider: _router.routeInformationProvider,
+        routeInformationParser: _router.routeInformationParser,
+        routerDelegate: _router.routerDelegate,
+      );
+
+  final _router = GoRouter(routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => GoRouterHome(),
+    ),
+    GoRoute(
+      path: '/1',
+      builder: (context, state) => GoRouter1(),
+    ),
+  ]);
 }
