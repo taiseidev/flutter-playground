3a5fafcc3b5fc4606940a6e21382c2de0777f321
onishi taisei <taisei.developer@gmail.com>
2022-06-26 10:13:06 +0900

    🚀 feat: create file for gyrosensor

---
+++ lib/presentasion/pages/gyrosensor/gyrosensor_home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// ジャイロセンサーで3D効果を与える
+// 考え方（3つのレイヤーに分ける）
+// 背面→画像
+// 中間→テキスト
+// 前面→画像
+// ジャイロセンサーで背面と前面を逆方向に移動させる。
+
+class GyrosensorHome extends StatelessWidget {
+  const GyrosensorHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
