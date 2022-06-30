f52eb0551825bad546591574a0a6d6281c6dea30
onishi taisei <taisei.developer@gmail.com>
2022-06-30 23:32:13 +0900

    masonを使用して自動でStatelessファイルを作成できるように実装

---
+++ bricks/my_stateless_widget/__brick__/{{name.snakeCase()}}.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class {{name.pascalCase()}} extends StatelessWidget {
+  const {{name.pascalCase()}}({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentasion/pages/mason_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MasonTest extends StatelessWidget {
+  const MasonTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
