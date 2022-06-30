59fe3076170ac457b5f5908156c569eaed800796
onishi taisei <taisei.developer@gmail.com>
2022-07-01 08:28:05 +0900

    🚀 feat: remove brick file and create file using mason

---
+++ lib/todo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Todo extends StatelessWidget {
+  const Todo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
