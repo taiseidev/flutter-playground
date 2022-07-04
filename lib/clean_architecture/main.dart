88a75f99ccc4087621aa963011ea81b446ba7348
onishi taisei <taisei.developer@gmail.com>
2022-07-04 21:20:16 +0900

    クリーンアーキテクチャ学習用のmainファイルを作成

---
+++ lib/clean_architecture/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MySampleApp(),
+    );
+  }
+}
+
+class MySampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
