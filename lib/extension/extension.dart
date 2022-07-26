a15925ecfaa4b8687f3a07f0050b3eeb7369ce32
onishi taisei <taisei.developer@gmail.com>
2022-07-26 22:33:48 +0900

    extension学習用のファイルを生成

---
+++ lib/extension/extension.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ExtensionSample(),
+    );
+  }
+}
+
+class ExtensionSample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
