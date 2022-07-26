6534af146c7172b7fcc21e0a21eefd20f681c701
onishi taisei <taisei.developer@gmail.com>
2022-07-26 22:36:59 +0900

    context拡張用のextensionを定義

--- lib/extension/extension.dart
+++ lib/extension/extension.dart
@@ -27,3 +27,20 @@ class ExtensionSample extends StatelessWidget {
     return Scaffold();
   }
 }
+
+extension ContextEx on BuildContext {
+  TextTheme get textTheme => Theme.of(this).textTheme;
+
+  ColorScheme get colorTheme => Theme.of(this).colorScheme;
+
+  Size get screenSize => MediaQuery.of(this).size;
+
+  double get screenHeight => MediaQuery.of(this).size.height;
+
+  double get screenWidth => MediaQuery.of(this).size.width;
+
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnack(
+      SnackBar snackBar) {
+    return ScaffoldMessenger.of(this).showSnackBar(snackBar);
+  }
+}
