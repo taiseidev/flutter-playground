0210e939c79dbc111b7ea2cadde35744535f87c2
onishi taisei <taisei.developer@gmail.com>
2022-07-28 23:44:10 +0900

    @immutableアノテーションをつけることによってimmutableの要件を満たしていないときにコンパイルエラーを吐くなど、immutableクラスを作る際は付けておいた方が良い

--- lib/immutable/learn_immutable.dart
+++ lib/immutable/learn_immutable.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class Mutable {
   Mutable(this.value);
   int value;
@@ -5,5 +7,15 @@ class Mutable {
 
 class Immutable {
   Immutable(this.value);
+  final int value;
+}
+
+// @immutableアノテーション付ける
+@immutable
+class Immutable2 {
+  // 全てのフィールドがfinalだと生成後に不変であることが保証されるのでconstにできる。
+  // (@immutableにより、constを付けないと警告がでて指定漏れを教えてくれる)
+  const Immutable2(this.value);
+  // finalが欠けるとimmutableとして成り立たないのでコンパイルエラー
   final int value;
 }
